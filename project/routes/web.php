8428f6de5b3f5ebb6c4772c1810afe699f85894d
jg-manalo <jg.manalo@outlook.com>
2025-03-27 15:10:02 +0800

    added basic backend using bootstrap

--- project/routes/web.php
+++ project/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
